5d3003ce33ff2e527de923581f9badf00324dbc2
Vladislav Plotnikov <vladik1209@gmail.com>
2022-09-07 19:18:08 +0300

    delete scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source devel/setup.bash
-catkin_make -DCATKIN_BLACKLIST_PACKAGES="stingray_gazebo_communication"
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source devel/setup.bash
-roslaunch sauvc_startup sauvc_master.launch qr_launch:=true
