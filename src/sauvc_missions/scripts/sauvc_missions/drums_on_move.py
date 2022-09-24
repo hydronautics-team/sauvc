200e9a09eb55d0e4285829d0ccc329e988f4b42d
Vladislav Plotnikov <vladik1209@gmail.com>
2022-09-24 20:27:38 +0300

    drums drop

--- src/sauvc_missions/scripts/sauvc_missions/drums_on_move.py
+++ src/sauvc_missions/scripts/sauvc_missions/drums_on_move.py
@@ -40,7 +40,7 @@ class DrumsMission(SAUVCMission):
             confirmation=self.confirmation,
             confidence=self.confidence,
             rotate=rotate,
-            )
+        )
 
         self.centering_submachine = CenteringWithAvoidSub(
             PureStateMachine.construct_name('CenteringOnMove', name),
@@ -54,49 +54,52 @@ class DrumsMission(SAUVCMission):
             wait=4,
             speed=0.2,
             is_big_w=0.2,
-            )
+        )
 
         self.centering_bottom_submachine = CenteringPlanarSub(
-            'planar',
+            PureStateMachine.construct_name('CenteringPlanarDrum', name),
             bottom_camera,  # bottom needed
             'blue_bowl',
             auv=auv,
-            )
+        )
 
         super().__init__(name, auv, camera, '', verbose=verbose)
 
     def setup_states(self):
-        return ('move_march', 'condition_search_drums', 'condition_centering_drums', 'custom_stop', 'custom_centering', 'custom_drop')
+        return ('move_march', 'condition_search_drums', 'condition_centering_drums', 'custom_stop', 'custom_centering', 'custom_drop', 'move_back')
 
     def setup_transitions(self):
         transitions = [
-            [self.machine.transition_start, self.machine.state_init, 'condition_search_drums'],
+            [self.machine.transition_start,
+                self.machine.state_init, 'condition_search_drums'],
 
             ['condition_f', 'condition_search_drums', self.machine.state_aborted],
             ['condition_s', 'condition_search_drums', 'condition_centering_drums'],
 
-            ['condition_f', 'condition_centering_drums', 'condition_centering_drums'],
+            ['condition_f', 'condition_centering_drums',
+                'condition_centering_drums'],
             ['condition_s', 'condition_centering_drums', 'move_march'],
 
             ['go_centering', 'move_march', 'custom_drop'],
 
-            [self.machine.transition_end, 'custom_drop', self.machine.state_end],
+            ['go_centering', 'custom_drop', 'move_back'],
+
+            ['go_centering', 'move_back', 'custom_stop'],
+
+            [self.machine.transition_end, 'custom_stop', self.machine.state_end],
         ]
         return transitions
-    
-    def drop(self):
-        self.machine.auv.execute_dropper_goal()
 
-    def dropper_close(self):
-        self.machine.auv.execute_dropper_goal(velocity=0)
-    
+    def drop(self):
+        self.machine.auv.execute_dropper_goal(velocity=50)
+
     def lift(self):
         self.machine.auv.execute_lifter_goal(
             {
                 "lift": True,
             }
         )
-    
+
     def lower(self):
         self.machine.auv.execute_lifter_goal(
             {
@@ -106,14 +109,6 @@ class DrumsMission(SAUVCMission):
         )
 
     def prerun(self):
-        self.lower()
-        rospy.sleep(5)
-        self.lift()
-        rospy.sleep(5)
-
-        self.drop()
-        rospy.sleep(5)
-        self.dropper_close()
         self.enable_object_detection(self.front_camera, True)
         self.machine.auv.execute_dive_goal({
             'depth': 600,
@@ -165,6 +160,12 @@ class DrumsMission(SAUVCMission):
                 'subFSM': True,
                 'custom': self.centering_bottom_submachine,
                 'args': ()
+            },
+            'move_back': {
+                'march': -0.5,
+                'lag': 0.0,
+                'yaw': 0,
+                'wait': 5,
             },
         }
         return scene
