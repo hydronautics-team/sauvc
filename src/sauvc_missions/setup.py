a08565fb56eb07a8048598be2fc026435e685e50
Vladislav Plotnikov <vladik1209@gmail.com>
2024-03-17 19:01:50 +0300

    update mission qualification

--- src/sauvc_missions/setup.py
+++ src/sauvc_missions/setup.py
@@ -1,12 +1,29 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'sauvc_missions'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['sauvc_missions'],
-    package_dir={'': 'scripts'},
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/configs/missions', glob('configs/missions/*.yaml')),
+        (f'share/{package_name}/configs/scenarios', glob('configs/scenarios/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vladushked',
+    maintainer_email='vladik1209@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
-
-setup(**setup_args)
