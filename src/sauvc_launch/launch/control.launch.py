b5b331ee3dd3576fd0752fde703b10b5f042624e
Vladislav Plotnikov <vladik1209@gmail.com>
2025-03-12 14:16:23 +0800

    fix control launch

--- src/sauvc_launch/launch/control.launch.py
+++ src/sauvc_launch/launch/control.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # missions
     mission_package_names_arg = DeclareLaunchArgument(
-        "mission_package_names", default_value='[sauvc_missions stingray_missions]'
+        "mission_package_names", default_value='[sauvc_missions, stingray_missions]'
     )
     transition_srv_arg = DeclareLaunchArgument(
         "transition_srv", default_value='/stingray/services/transition'
